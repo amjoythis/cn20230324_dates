67022593099d7e6cc5aeb1661e15837e4590abc8
Artur Marques <am@arturmarques.com>
2023-03-24 13:18:14 +0000

    1st working version

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # app.py
-from flask import Flask, request
+from flask import Flask, request, render_template
 from tools import str_to_date,\
     diff_between_dates
 from datetime import datetime
@@ -9,19 +9,29 @@ app = Flask(__name__)
 HTML_NAME_FOR_THE_DATE = "d"
 
 # decorator
-@app.route("/distance", methods=['POST'])
+@app.route("/", methods=['POST', 'GET'])
+
 def respond_to_distance_request():
-    the_user_date:str =\
-        request.form[HTML_NAME_FOR_THE_DATE]
+    b_there_is_date:bool =\
+        HTML_NAME_FOR_THE_DATE in request.form.keys()
 
-    the_date:datetime = \
-        str_to_date(the_user_date)
+    if (b_there_is_date):
+        the_user_date:str =\
+            request.form[HTML_NAME_FOR_THE_DATE]
 
-    response:dict = diff_between_dates(
-        the_date
-    )
+        the_date:datetime = \
+            str_to_date(the_user_date)
 
-    return response
+        response:dict = diff_between_dates(
+            the_date
+        )
+
+        return response
+    #
+    else:
+        return render_template(
+            "date_input.html"
+        )
 # def respond_to_distance_request
 
 app.run(
